380ae3f6c49e123d7f036713143c8ba7425e2982
kwdaaa <kwd.aaa.a@icloud.com>
2020-10-22 17:57:56 +0900

    １回目の課題提出

--- server/routes/web.php
+++ server/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// @で実行したい関数の指定をする。
+// http://localhost/calcs/{num1}/{calc}/{num2}のURLにアクセスがあった場合、MessageControllerのshow関数を実行
+// {num1}と{num2}には数字が入る。
+// {calc}には addition / subtraction / multiplication / divisionのいずれかが入る。
+// 例：http://localhost/calcs/1/addition/2
+Route::get('calcs/{num1}/{what}/{num2}', 'MessageController@calc');
